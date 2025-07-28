a2d7756be80c8b60ef726fe3ddf8f7196860021e
Ajat Darojat <ajatdarojat45@gmail.com>
2025-07-28 08:27:42 +0700

    chore: change connectionName to name and connectionUri to connection

--- src/interfaces/mongoloquent-options.interface.ts
+++ src/interfaces/mongoloquent-options.interface.ts
@@ -4,22 +4,22 @@ import { Model } from "mongoloquent";
 export type IMongoloquentModelClass<T = any> = new (...args: any[]) => Model<T>;
 
 export interface IMongoloquentModuleOptions {
-  connectionUri: string;
+  name?: string;
+  connection: string;
   database: string;
   models?: IMongoloquentModelClass<any>[];
-  connectionName?: string;
   timezone?: string;
   global?: boolean;
 }
 
 export interface IMongoloquentModuleAsyncOptions extends Pick<ModuleMetadata, "imports"> {
-  connectionName?: string;
+  name?: string;
   models?: IMongoloquentModelClass<any>[];
-  global?: boolean;
-  useFactory?: (
+  useFactory: (
     ...args: any[]
   ) =>
-    | Promise<Pick<IMongoloquentModuleOptions, "connectionUri" | "database" | "timezone">>
-    | Pick<IMongoloquentModuleOptions, "connectionUri" | "database" | "timezone">;
+    | Promise<Pick<IMongoloquentModuleOptions, "connection" | "database" | "timezone">>
+    | Pick<IMongoloquentModuleOptions, "connection" | "database" | "timezone">;
+  global?: boolean;
   inject?: any[];
 }

e999ce71d8e83e6b2e39f4d2265aba3df4a92484
Ajat Darojat <ajatdarojat45@gmail.com>
2025-08-12 18:12:32 +0700

    feat: add MONGOLOQUENT_TRANSACTIONAL constant and fix formatting in getDynamicDB function

--- src/common/mongoloquent.utils.ts
+++ src/common/mongoloquent.utils.ts
@@ -2,6 +2,7 @@ import { DB } from "mongoloquent";
 
 export const MONGOLOQUENT_MODULE = "MONGOLOQUENT_MODULE";
 export const MONGOLOQUENT_DB = "MONGOLOQUENT_DB";
+export const MONGOLOQUENT_TRANSACTIONAL = "MONGOLOQUENT_TRANSACTIONAL";
 
 export function getMongoloquentModuleToken(name: string = "default"): string {
   return `${MONGOLOQUENT_MODULE}_${name}`;
